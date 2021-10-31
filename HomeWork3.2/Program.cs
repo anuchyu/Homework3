9198d24a5e757a69b60a5ab1cd132f6b999dfa42
User <User@JULIA>
2021-10-31 16:51:42 +0700

    Добавьте файлы проекта.

---
+++ HomeWork3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветстую вас в игре Blackjack. Какое количество карт у вас на руках?");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i=0; i<number; i++ )
+            {
+                Console.WriteLine(" Введите номинал каждой карты. Обращаю ваше внимание: \n Валет - J \n Дама - Q \n Король - К \n Туз - Т");
+                String Index = Console.ReadLine();
+                    if (Index == "J" || Index == "Q" || Index == "K" || Index == "T")
+                    {
+                        sum =+ 10;
+                    }
+                    else
+                    {
+                        int card = Convert.ToInt32(Index);
+                        sum = + card;
+                    }
+            }
+            Console.WriteLine($"Сумма значений карт ровна = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Число {number} четное");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} нечётное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
